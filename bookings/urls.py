cee5718d48cb42b7a6a425d861d305a20fb3ef89
François Rejeté <francois@rejete.com>
2016-05-10 19:21:39 +0900

    first shot at dashboard

--- bookings/urls.py
+++ bookings/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
 from djgeojson.views import GeoJSONLayerView
 
+from . import views
 from .models import Booking
 
 urlpatterns = [
+    url(r'^$', views.dashboard, name='dashboard'),
     url(
         r'^data.geojson$',
         GeoJSONLayerView.as_view(
--- bookings/views.py
+++ bookings/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.db.models import Count
 
-# Create your views here.
+from timesdata.models import Station
+
+
+def dashboard(request):
+    context = {
+        'stations': Station.objects.all().annotate(nb_bookings=Count('bookings__booking_id')).filter(nb_bookings__gt=1).order_by('-nb_bookings'),
+    }
+    return render(request, 'bookings/dashboard.html', context)
